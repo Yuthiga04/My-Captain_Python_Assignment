ed59587dfca4295868d1b89561117095eec82a58
Yuthiga04 <124693783+Yuthiga04@users.noreply.github.com>
2023-02-09 20:11:09 +0530

    Add files via upload

---
+++ My Captain Assignment2.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  9 19:59:32 2023
+
+@author: 65820
+"""
+
+l1=eval(input('Enter a list:'))
+for i in range(len(l1)):
+    if l1[i]>0:
+        print(l1[i])
